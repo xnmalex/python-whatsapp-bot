30fe89bc1805d0a4b49e5bb471c22c66b13d0e9b
Alex Gui <xnmalex@gmail.com>
2025-04-30 00:11:00 +0800

    added subscription to profile resp

--- app/routes/profile_routes.py
+++ app/routes/profile_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from app.db.user_dao import get_user_by_id, delete_user
+from app.db.user_subscription_dao import get_user_subscription
 import jwt
 import os
 
@@ -21,13 +22,16 @@ def get_profile():
     if not user_id:
         return jsonify({"error": "Invalid or missing token"}), 401
 
-    user = get_user_by_id(user_id)
-    if not user:
-        return jsonify({"error": "User not found"}), 404
-
-    user.pop("password", None)
-    return jsonify({"user": user})
-
+    try:
+        user = get_user_by_id(user_id)
+        subscription = get_user_subscription(user_id)
+        if not user:
+            return jsonify({"error": "User not found"}), 404
+        user.pop("password", None)
+        return jsonify({"user": user, "subscription":subscription})
+    except Exception as e:
+         return jsonify({"status": False, "error":f"{e}"})
+    
 
 # Admin-only route
 @profile_blueprint.route("/admin/delete-user/<user_id>", methods=["DELETE"])
